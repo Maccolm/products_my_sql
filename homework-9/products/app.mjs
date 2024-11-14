f6362d53d84d407ca28d94a3a9c8e11982a21702
Vitalii Shapovalov <martyit@ukr.net>
2024-11-14 12:37:34 +0100

    product task made

--- homework-9/products/app.mjs
+++ homework-9/products/app.mjs
@@ -4,7 +4,9 @@ import cookieParser from "cookie-parser";
 import logger from "morgan";
 import { fileURLToPath } from "url";
 import session from 'express-session'
-import { title } from "process";
+import FormValidator from './validators/formValidator.mjs'
+import { checkSchema, validationResult } from 'express-validator'
+
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the
@@ -27,30 +29,53 @@ app.use(
 
 app.use(express.static(path.join(__dirname, "public")));
 
+const initialProducts = [
+	{ pr_type: 'Name', title: "Apple", price: 10, quantity: 50 },
+	{ pr_type: 'Name', title: "Banana", price: 5, quantity: 30 },
+	{ pr_type: 'Name', title: "Orange", price: 8, quantity: 20 },
+	{ pr_type: 'Name', title: "Protein", price: 670, quantity: 5 },
+	{ pr_type: 'Name', title: "Gainer", price: 470, quantity: 4 },
+];
+
 // -------login---------------
-app.post('/login', function (req, res) {
+app.post('/login', checkSchema(FormValidator.formSchema), function (req, res) {
 	console.log(req.body)
 
+	const error = validationResult(req)
+	if(!error.isEmpty()) {
+		return res.status(400).json({ errors: error.array() })
+	}
 	res.cookie('userName', req.body.username, { maxAge: 600000 })
-	res.redirect('/')
+	res.redirect('/products?sort=price_low')
 	
 })
 //--------read--------
-app.get('/', function (req, res) {
+app.get('/products', function (req, res) {
 	console.log('Cookies ', req.cookies)
 
 	if(req.session.isNew || !req.session.products) {
-		req.session.products = []
-		res.render('products', {title: 'shop card is empty', products: [] })		
-	} else {
-		res.render('products', { title: 'shop card', products: req.session.products })
+		req.session.products = [...initialProducts]
 	}
+	const { sort } = req.query
+	let products = [...req.session.products]
+	if(sort === 'price_low'){
+		products.sort((a, b) => a.price - b.price)
+	} else if(sort === 'price_high') {
+		products.sort((a, b) => b.price - a.price)
+	}
+
+	res.render('products', {title: 'Products', products })		
+	})
+//-----------middleware-----------
+app.post('/addProduct', (req, res) => {
+	req.session.products.push({ pr_type: 'Name', title: req.body.product, price: req.body.price, quantity: req.body.quantity })
+	res.redirect('/products?sort=price_high')
 })
 //-------logout------------
-app.get('/logout', function (req, res) {
+app.post('/logout', function (req, res) {
 	if(req.cookies.userName)
 		res.clearCookie('userName')
-	res.redirect('/')
+	res.redirect('loginForm.html')
 })
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
